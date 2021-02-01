ab88983e71ff060d1c565dbb8cf14e2df0372efb
kiranmukherjee <77300690+kiranmukherjee@users.noreply.github.com>
2021-02-01 12:48:47 -0800

    staring

---
+++ script.js
@@ -0,0 +1,18 @@
+// let steve= {
+//     desire: "Wants to talk"
+// }
+// let john= {
+//     desire: "Does not want to talk"
+// }
+// let james= {
+//     desire: "Does not want to talk"
+// }
+// let david= {
+//     desire: "Wants to talk"
+// }
+
+class person {
+    constructor(desire){
+        this.desire= wants 
+    }
+}
